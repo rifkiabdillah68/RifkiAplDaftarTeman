b7944021ddd194024e92e13bc2496ee0967275e3
rifkiabdillah68 <rifkiabdillah68@gmail.com>
2021-06-04 03:50:43 +0700

    aplikasi daftar teman
    aplikasi daftar teman

---
+++ app/src/main/java/com/example/aplikasidaftarteman/data_teman.kt
@@ -0,0 +1,16 @@
+package com.example.aplikasidaftarteman
+
+class data_teman {
+    var nama : String? = null
+    var alamat : String? = null
+    var noHp : String? = null
+    var key : String? = null
+
+    constructor()
+
+    constructor(nama: String?, alamat: String?, noHp: String?) {
+        this.nama = nama
+        this.alamat = alamat
+        this.noHp = noHp
+    }
+}
